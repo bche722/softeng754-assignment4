e4d80c401a74493c31536eb19a6fe5c11c96fb8d
Vincent <bche722@aucklanduni.ac.nz>
2019-05-10 15:25:18 +1200

    create a maven project

---
+++ src/main/java/nz/ac/auckland/softeng754_assignment4/App.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.softeng754_assignment4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
