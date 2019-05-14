8e9acc53d6a3f93304e92bd374bc94ef9afaddb5
jren791 <jren791@aucklanduni.ac.nz>
2019-05-14 23:35:18 +1200

    Update createPullRequest testcase.

--- src/test/java/nz/ac/auckland/softeng754_assignment4/service/GithubServiceTest.java
+++ src/test/java/nz/ac/auckland/softeng754_assignment4/service/GithubServiceTest.java
@@ -1,12 +1,17 @@
 package nz.ac.auckland.softeng754_assignment4.service;
 
 import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.egit.github.core.User;
 import org.eclipse.egit.github.core.PullRequest;
+import org.eclipse.egit.github.core.PullRequestMarker;
 import org.eclipse.egit.github.core.RepositoryContents;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.SearchRepository;
 
 import javax.ws.rs.NotAuthorizedException;
 
@@ -15,15 +20,15 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 public class GithubServiceTest {
-	
+
 	String username;
 	String password;
 	IGitHubService github;
 	PullRequest pullRequest;
-	
+
 	@Before
 	public void SetUp() {
-		//Given
+		// Given
 		username = "Saltedfish754@gmail.com";
 		password = "S@lted_754";
 		github = Mockito.mock(IGitHubService.class);
@@ -32,45 +37,53 @@ public class GithubServiceTest {
 
 	@Test
 	public void shouldReturnAuthWithCorrectUsernameAndPassword() {
-		
+
 		// Given
 		User user = new User();
 		user.setLogin(username);
 		Mockito.doReturn(user).when(github).signIn(username, password);
-		
+
 		// When
 		github.signIn(username, password);
-		
+
 		String login = user.getLogin();
-		
+
 		// Then
 		assertEquals(login, username);
 	}
-	
+
 	@Test(expected = NotAuthorizedException.class)
 	public void shouldThrowExceptionWithIncorrectyUsernameAndPassword() {
 
 		// Given
 		String wrong_password = "Wrong Password";
 		Mockito.doThrow(NotAuthorizedException.class).when(github).signIn(username, wrong_password);
-		
+
 		// When
 		github.signIn(username, wrong_password);
-		
+
 	}
-	
+
 	@Test
-	public void shouldReturnPullRequestWhenCreatePullRequest() {
+	public void shouldReturnPullRequestWhenCreatePullRequest() throws IOException {
 		
 		// Given
 		String title = "Amazing new feature";
 		String head = "softeng754-assignment4:new-feature";
 		String base = "milestone-2";
 		String body = "Please pull this in!";
-		Mockito.doReturn(pullRequest).when(github).createPullRequest(title, head, base, body);
+		PullRequest request = new PullRequest();
+		request.setTitle(title);
+		request.setBody(body);
+		request.setHead(new PullRequestMarker().setRef(head));
+		request.setBase(new PullRequestMarker().setRef(base));
+		String repoOwner = "bche722";
+		String repoName = "softeng754-assignment4";
+		RepositoryId repositoryId = new RepositoryId(repoOwner, repoName);
+		Mockito.doReturn(pullRequest).when(github).createPullRequest(repositoryId, request);
 		
 		// When
-		PullRequest result = github.createPullRequest(title, head, base, body);
+		PullRequest result = github.createPullRequest(repositoryId, request);
 		
 		// Then
 		assertEquals(result, pullRequest);
