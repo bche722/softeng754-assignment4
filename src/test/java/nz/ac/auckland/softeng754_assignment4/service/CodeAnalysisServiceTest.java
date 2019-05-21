13c88e99528535beeb0eaa838a9b2f8551888b47
jren791 <jren791@aucklanduni.ac.nz>
2019-05-21 23:47:54 +1200

    Cleanup testcase.

--- src/test/java/nz/ac/auckland/softeng754_assignment4/service/CodeAnalysisServiceTest.java
+++ src/test/java/nz/ac/auckland/softeng754_assignment4/service/CodeAnalysisServiceTest.java
@@ -1,12 +1,10 @@
 package nz.ac.auckland.softeng754_assignment4.service;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
