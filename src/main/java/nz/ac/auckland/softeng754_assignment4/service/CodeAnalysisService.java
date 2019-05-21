024c0cd251c03a9473dd1a37718f87e5a0dc90b2
jren791 <jren791@aucklanduni.ac.nz>
2019-05-21 23:48:06 +1200

    Implemented CodeAnaylsisService.

--- src/main/java/nz/ac/auckland/softeng754_assignment4/service/CodeAnalysisService.java
+++ src/main/java/nz/ac/auckland/softeng754_assignment4/service/CodeAnalysisService.java
@@ -1,19 +1,35 @@
 package nz.ac.auckland.softeng754_assignment4.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import nz.ac.auckland.softeng754_assignment4.model.CodeAbstraction;
 import nz.ac.auckland.softeng754_assignment4.model.CodeAnalysisReport;
+import nz.ac.auckland.softeng754_assignment4.model.CodeAnomaly;
+
 import org.eclipse.egit.github.core.RepositoryContents;
 
 public class CodeAnalysisService implements ICodeAnalysisService
 {
+    ICodeInspectionService inspectionService;
+
     @Override
     public CodeAnalysisReport analysis(List<RepositoryContents> contents) {
-      return null;
+        List<CodeAnomaly> anomalies = new ArrayList<CodeAnomaly>();
+        List<String> fileContents = new ArrayList<String>();
+        contents.forEach(content -> {
+          String fileContent = content.getContent();
+          fileContents.add(fileContent);
+          List<CodeAnomaly> result = inspectionService.inspector(content);
+          anomalies.addAll(result);
+        });
+        CodeAbstraction abstraction = new CodeAbstraction(fileContents);
+        CodeAnalysisReport report = new CodeAnalysisReport(abstraction, anomalies);
+        return report;
     }
 
     @Override
     public void setInspectionService(ICodeInspectionService service) {
-
+        this.inspectionService = service;
     }
 }
